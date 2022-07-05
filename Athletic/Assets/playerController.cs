ca9fb635ea493f64f61a1eb10f55aa5c97a45bbd
syosinsya <komattarakoredaze@gmail.com>
2022-07-05 13:05:58 +0900

    save load

--- Athletic/Assets/playerController.cs
+++ Athletic/Assets/playerController.cs
@@ -10,6 +10,7 @@ public class playerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        init();//イニシャライズ
         LoadPosition();//初回ロード
     }
 
@@ -22,12 +23,26 @@ public class playerController : MonoBehaviour
     }
     void OnTriggerEnter(Collider other) {
         Debug.Log(other.tag);
+        switch(other.tag){
+            case "flag":
+                SavePosition();
+                break;
+        }
     }
-
+    void init(){
+        PlayerPrefs.SetFloat("savedPosX", -35f);
+        PlayerPrefs.SetFloat("savedPosY", 1.37f);
+        PlayerPrefs.SetFloat("savedPosZ", -3f);
+    }
+    void SavePosition(){
+        PlayerPrefs.SetFloat("savedPosX", transform.position.x);
+        PlayerPrefs.SetFloat("savedPosY", transform.position.y);
+        PlayerPrefs.SetFloat("savedPosZ", transform.position.z);
+    }
     void LoadPosition(){
-        savedPosX = PlayerPrefs.GetFloat("savedPosX", -35f);
-        savedPosY = PlayerPrefs.GetFloat("savedPosY", 1.37f);
-        savedPosZ = PlayerPrefs.GetFloat("savedPosZ", -3f);
+        savedPosX = PlayerPrefs.GetFloat("savedPosX");
+        savedPosY = PlayerPrefs.GetFloat("savedPosY");
+        savedPosZ = PlayerPrefs.GetFloat("savedPosZ");
         transform.position = new Vector3(savedPosX,savedPosY,savedPosZ);
     }
 }
